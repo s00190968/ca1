f0c39a823777fe96e6472d0b7eb018357c03ab04
GilleMochi <janessa.aulen@gmail.com>
2018-11-07 16:11:04 +0000

    Finished the CA1 sheet.

--- MemberTest/member.cs
+++ MemberTest/member.cs
@@ -6,27 +6,73 @@ using System.Threading.Tasks;
 
 namespace MemberTest
 {
-    class member
+    class Member:IComparable
     {
+        public static int memberCount = 0;
+
         public string firstname { get; set; }
         public string lastname { get; set; }
         public string memberId { get; set; }
         public string membershipStatus { get; set; }
         public int age { get; set; }
-
-        public member(string firstanem, string lastname, string mebershipId, int age)
+        public decimal price { get; }
+        
+        public Member(string firstname, string lastname, string mebershipId, int age)
         {
             this.firstname = firstname;
             this.lastname = lastname;
             this.memberId = memberId;
             this.age = age;
             membershipStatus = "Expired";
+
+            if(age < 18)
+            {
+                price = 50;
+            }
+            else
+            {
+                price = 150;
+            }
+            memberCount++;
         }
 
         public override string ToString()
         {
-            return string.Format("Firstname:{0} Lastname:{1} Age:{2} ID:{3} Status:{4}",firstname,lastname,age,memberId,membershipStatus);
+            return string.Format("\tFirstname: {0}, Lastname: {1}, Age: {2}, ID: {3}, Status: {4}, Price: {5}.", firstname, lastname, age, memberId, membershipStatus, price);
         }
 
+        public void changeMembershipStatus()
+        {
+            if(membershipStatus == "Expired")
+            {
+                membershipStatus = "Current";
+            }
+            else
+            {
+                membershipStatus = "Expired";
+            }
+        }
+
+        public int CompareTo(object obj)
+        {
+            Member other = (Member)obj;
+            ////lastname
+            //return this.lastname.CompareTo(other.lastname);
+
+            //age
+            if (this.age > other.age)
+            {
+                return 1;
+            }
+            else if (this.age < other.age)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
     }
 }
--- MemberTest/memberTest.cs
+++ MemberTest/memberTest.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace MemberTest
 {
-    class memberTest
+    class MemberTest
     {
+        static void Main(string[] args)
+        {
+            Member[] Members = new Member[] { new Member("Name 1", "Lastname 1", "1234", 30),
+                new Member("Name 2", "ALastname 2", "1235a", 13),
+                new Member("Name 3", "TheLastname 3", "1236a", 40),
+                new Member("Name 4", "SomeLastname 4", "1237a", 30),
+                new Member("Name 5", "OtherLastname 5", "1238a", 27)};
+
+            displayAllMembers(Members);
+
+            Members[1].changeMembershipStatus();
+            Members[3].changeMembershipStatus();
+            Array.Sort(Members);
+
+            displayAllMembers(Members);
+
+            Array.Reverse(Members);
+            displayAllMembers(Members);
+        }
+
+        static void displayAllMembers(Member[] membersArr)
+        {
+            Console.WriteLine(" All members' information:");
+            Console.WriteLine(string.Format(" {0} members in total.\n", Member.memberCount));
+            foreach (Member item in membersArr)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+        }
     }
 }
